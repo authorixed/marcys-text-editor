4ff122fa9dbddf4463b95ec58b8ea81b469cd0b0
Josh Levy <joshlevycatches@gmail.com>
2024-11-17 22:59:30 -0500

    My give up commit.

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -4,29 +4,29 @@ const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './Develop/client/src/js/index.js', // Entry file
+  entry: './Develop/client/src/js/index.js', // Full path to the entry point
   output: {
     filename: 'main.js',
-    path: __dirname + '/dist',
-    clean: true,
+    path: __dirname + '/dist', // Output directory
+    clean: true, // Cleans old files in the output directory
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './Develop/client/index.html', // HTML template
+      template: './Develop/client/index.html', // Full path to the HTML template
     }),
     new MiniCssExtractPlugin(),
-    new WorkboxPlugin.GenerateSW(),
+    new WorkboxPlugin.GenerateSW(), // Automatically generates service worker
   ],
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: 'babel-loader',
+        use: 'babel-loader', // Converts modern JS to older versions
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'], // Handles CSS files
       },
     ],
   },
